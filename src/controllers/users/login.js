6677a378f909975a8ad3c4b858e3e973d73085f5
Thiago Maia <thiagokcio@hotmal.com>
2023-11-21 17:19:36 -0300

    criacao de endpoint para fazer login de usuario

---
+++ src/controllers/users/login.js
@@ -0,0 +1,45 @@
+const knex = require("../../../services/knex");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+    const { email, senha } = req.body;
+
+    try {
+        const userFound = await knex("usuarios")
+            .where({ email })
+            .first();
+
+        if (!userFound) {
+            return res
+                .status(404)
+                .json("Email e/ou senha incorretos.");
+        }
+
+        const validatePassword = await bcrypt.compare(senha, userFound.senha);
+
+        if (!validatePassword) {
+            return res
+                .status(400)
+                .json("Email e/ou senha incorretos.");
+        }
+
+        const token = jwt.sign(
+            { id: userFound.id }, 
+            process.env.SENHA_JWT, 
+            {expiresIn: "1h"}
+        );
+
+        const { senha: _, ...userData } = userFound;
+
+        return res.status(200).json({
+            usuario: userData,
+            token
+        });
+        
+    } catch (error) {
+        return res.status(500).json("Erro no servidor.");
+    }
+};
+
+module.exports = loginUser;
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const registerUser = require('./controllers/users/register');
+const loginUser = require('./controllers/users/login');
 
 const router = express()
 
 router.post('/user', registerUser)
+router.post('/login', loginUser)
 
 module.exports = router
